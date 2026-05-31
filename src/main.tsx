5f0a064885a31cf8562f13c4fe09861267d1154f
Gab1nh0 <gabriel507xs@gmail.com>
2026-05-31 14:15:14 -0500

    tour archivos ajustados

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { LanguageProvider } from './context/LanguageContext.tsx';
-import Booking from './pages/fleet.tsx';
+import Booking from './pages/tours.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
